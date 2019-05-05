4b60e80dc7a6c85d75024d8af7b6dc9eb8267e2a
dsarachaga <dsarachaga@gmail.com>
2019-05-05 13:21:11 -0400

    Initial commit from laptop

--- MSDS Data Wrangling/Final Project/Sarachaga-ds1591-finalProject.R
+++ MSDS Data Wrangling/Final Project/Sarachaga-ds1591-finalProject.R
@@ -1,1 +1,10 @@
+#Data Wrangling project
+
+#websites
+# https://ritetag.com/best-hashtags-for/shopping
+# https://developer.twitter.com/en/apps
+# https://developer.twitter.com/en/account/get-started
+# https://towardsdatascience.com/access-data-from-twitter-api-using-r-and-or-python-b8ac342d3efe
+# https://rpubs.com/chrisbrunsdon/twitterclouds
+
 
